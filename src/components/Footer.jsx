79605db10623d6b25cd8650368df040af7dfb7f7
pacifictones <brianwburk@gmail.com>
2024-09-01 15:53:15 +0700

    a few small cosmetic updates before adding to vercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-slate-200">
-      <p>footer</p>
+      <p className="p-2">crumbs... ... ... ...</p>
     </div>
   );
 };
