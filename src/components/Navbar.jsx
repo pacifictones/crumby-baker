88675b5ddd83ca451a1a3d008cd4653e58117018
pacifictones <brianwburk@gmail.com>
2024-11-27 15:34:40 +0400

    hamburger menu works and is absolute so it doesn't push doen main

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,32 +12,37 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full bg-gray-700">
-      <div className="container mx-auto px-4 py-2 flex items-center justify-between">
-        <h1 className="text-lg font-bold block sm:hidden">The Crumby Baker</h1>
-        {/* Hamburger Icon */}
-        <button
-          className="block sm:hidden text-white focus:outline-none"
-          onClick={toggleMenu}
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http:/www.w3.org/2000/svg"
+    <nav className="relative bg-gray-700">
+      <div className="container mx-auto px-4 py-2">
+        {/* Small Screen Layout */}
+        <div className="flex items-center justify-between sm:hidden">
+          {/* Title (Visible on Small Screens) */}
+          <h1 className="text-lg font-bold">The Crumby Baker</h1>
+
+          {/* Hamburger Menu */}
+          <button
+            className="block sm:hidden text-white focus:outline-none bg-transparent border-0"
+            onClick={toggleMenu}
           >
-            <path
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth={2}
-              d={isOpen ? "M6 18L18 6M6 6L18 18" : "M4 6h16M4 12h16M4 18h16"}
-            />
-          </svg>
-        </button>
-        {/* Full Menu (visible on Large Screens) */}
+              viewBox="0 0 24 24"
+              xmlns="http:/www.w3.org/2000/svg"
+            >
+              <path
+                d={isOpen ? "M6 18L18 6M6 6L18 18" : "M4 6h16M4 12h16M4 18h16"}
+              />
+            </svg>
+          </button>
+        </div>
 
-        <ul className=" hidden sm:grid grid-cols-5 sm:gap-2 text-center text-white">
+        {/* Full Menu (visible on Large Screens) */}
+        <ul className=" hidden sm:flex justify-center gap-8 text-white">
           <li>
             <Link className=" block py-1 sm:py-2 " to="/">
               Home
@@ -65,9 +70,10 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+
       {/* Dropdown Menu (visible on Small Screens) */}
       {isOpen && (
-        <ul className="sm:hidden bg-gray-800 text-white text-center max-h-64 overflow-y-auto">
+        <ul className=" absolute top-full left-0 w-full  bg-gray-800 text-white text-center z-50 shadow-lg sm:hidden">
           <li>
             <Link className="block py-2" to="/" onClick={closeMenu}>
               Home
