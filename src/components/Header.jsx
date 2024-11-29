c01d7c033056b7303395b992a3101e0197ee817d
pacifictones <brianwburk@gmail.com>
2024-11-29 20:45:46 +0400

    made header sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className=" bg-gray-800 text-white w-full">
+    <header className="sticky top-0 bg-gray-800 text-white w-full">
       <div className=" max-w-screen-lg mx-auto  sm:p-4 text-center sm:text-left">
         {/* Title disappears on small screens */}
         <h1 className="text-3xl font-bold hidden sm:block">The Crumby Baker</h1>
