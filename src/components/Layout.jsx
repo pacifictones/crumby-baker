f11481f9a2efa7ca369578f71606f72586003bf9
pacifictones <brianwburk@gmail.com>
2024-12-21 22:20:53 +0400

    corrected breadcrumbs typo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Breadcrumbs from "./BreadCrumbs";
+import Breadcrumbs from "./Breadcrumbs";
 
 const Layout = ({ children }) => {
   return (
