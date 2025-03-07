bed7685332dfd619de266b5acdf2b3887322c481
pacifictones <brianwburk@gmail.com>
2025-03-07 11:16:40 +0400

    tweaked data deletion page

--- src/pages/DataDeletion.jsx
+++ src/pages/DataDeletion.jsx
@@ -6,13 +6,16 @@ const DataDeletion = () => {
       <h1 className="text-2xl font-bold font-heading mb-4">
         Data Deletion Policy
       </h1>
-      <p className="font-body">Last updated: March 2025</p>
+      <p className="font-body mb-4">Last updated: March 2025</p>
       <p className="font-body">
         If you wish to delete your data from The Crumby Baker, please{" "}
         <Link className="font-semibold hover:text-brand-primary" to="/contact">
           contact us
-        </Link>
-        with the subject {"Data Deletion Request."}
+        </Link>{" "}
+        with the subject{" "}
+        <span className="text-brand-primary mb-4">
+          {"Data Deletion Request."}
+        </span>
       </p>
 
       <p className="font-body">We will process your request within 7 days.</p>
