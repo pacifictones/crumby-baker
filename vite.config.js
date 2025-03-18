70e29181400ce6b4d890736d36703722051dc56f
pacifictones <brianwburk@gmail.com>
2025-03-18 16:45:51 +0400

    killed react-snap as it was too buggy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "es2019",
-    outDir: "dist",
-  },
   test: {
     globals: true,
     environment: "jsdom",
